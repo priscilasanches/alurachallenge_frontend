11a46d745c61cabfd622dbf20830efcf5eb3791c
Priscila <psjanuario@gmail.com>
2021-12-12 15:09:23 -0300

    Reorganização e limpeza código

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const botaoMenu = document.querySelector('[data-botao-menu-mobile]');
-const botaFechar = document.querySelector('[data-botao-fechar]');
+const botaoMenu = document.querySelector('[data-botao-menu]');
 const menu = document.querySelector('[data-menu-mobile]');
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.add('menu-mobile--ativo');
-    botaoMenu.classList.add('botao-menu-mobile--esconder');
-    botaFechar.classList.add('botao-fechar-mobile--mostrar');
+    menu.classList.toggle('menu-mobile--ativo');
+    botaoMenu.classList.toggle('fa-times');
 });
 
-botaFechar.addEventListener('click', () => {
-    menu.classList.remove('menu-mobile--ativo');
-    botaoMenu.classList.remove('botao-menu-mobile--esconder');
-    botaFechar.classList.remove('botao-fechar-mobile--mostrar');
-})
