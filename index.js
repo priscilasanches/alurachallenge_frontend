dd25a3527de716705f0fd6ee44209326291ca998
Priscila <psjanuario@gmail.com>
2021-12-12 19:06:49 -0300

    Ajustes finais layout html e css

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const botaoMenu = document.querySelector('[data-botao-menu]');
 const menu = document.querySelector('[data-menu-mobile]');
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-mobile--ativo');
+    menu.classList.toggle('menu-mobile');
     botaoMenu.classList.toggle('fa-times');
 });
 
