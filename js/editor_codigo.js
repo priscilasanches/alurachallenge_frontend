1d776e4b4dde56a1c6db1a877bf4c03464deba32
Priscila <psjanuario@gmail.com>
2021-12-15 08:24:49 -0300

    Desenvolvimento página comunidade

--- js/editor_codigo.js
+++ js/editor_codigo.js
@@ -1,29 +1,8 @@
-const botaoMenu = document.querySelector('[data-botao-menu]');
-const menu = document.querySelector('[data-menu-mobile]');
-
-const botaoBusca = document.querySelector('[data-botao-busca]');
-const campoBusca = document.querySelector('[data-campo-busca]');
-const logo = document.querySelector('[data-logo]');
-
 const corInput = document.querySelector('[data-cor-input]');
 const boxEditor = document.querySelector('[data-box-editor]');
 
 const linguagem = document.querySelector('[data-linguagem]');
-const botaoHL = document.querySelector('[data-botao-hl]')
-
-//Menu tablet e mobile
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-mobile');
-    botaoMenu.classList.toggle('fa-times');
-});
-
-//Busca Mobile
-botaoBusca.addEventListener('click', () => {
-    botaoMenu.classList.toggle('hidden');
-    logo.classList.toggle('hidden');
-    botaoBusca.classList.toggle('fa-times');
-    campoBusca.classList.toggle('cabecalho__entrada--active');
-});
+const botaoHL = document.querySelector('[data-botao-hl]');
 
 //Altera cor no editor de código conforme escolha no color picker (outra opção de evento é o change, que altera a cor apenas após sair do color picker)
 corInput.addEventListener('input', () => {
@@ -41,8 +20,3 @@ function aplicaHighlight() {
 botaoHL.addEventListener ('click', () => {
     aplicaHighlight();
 })
-
-
-
-
-
---
+++ js/script.js
@@ -0,0 +1,27 @@
+const botaoMenu = document.querySelector('[data-botao-menu]');
+const menu = document.querySelector('[data-menu-mobile]');
+
+const botaoBusca = document.querySelector('[data-botao-busca]');
+const campoBusca = document.querySelector('[data-campo-busca]');
+const logo = document.querySelector('[data-logo]');
+
+//Menu tablet e mobile
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-mobile');
+    botaoMenu.classList.toggle('fa-times');
+});
+
+//Busca Mobile
+botaoBusca.addEventListener('click', () => {
+    botaoMenu.classList.toggle('hidden');
+    logo.classList.toggle('hidden');
+    botaoBusca.classList.toggle('fa-times');
+    campoBusca.classList.toggle('cabecalho__entrada--active');
+});
+
+
+
+
+
+
+
